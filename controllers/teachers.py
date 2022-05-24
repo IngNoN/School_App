64697ffbbbf69d4cde6b9e0f6261432d7703f0e2
Sebastian Karner <sebastian.karner@bulme.at>
2022-05-24 10:29:07 +0200

    n to n relation, start report file

--- controllers/teachers.py
+++ controllers/teachers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, request
+import sqlalchemy
 from forms.addTeacherForm import AddTeacherForm
-from models import GroupOfTeacher, Teacher, db
+from models import GroupOfTeacher, Teacher, TeacherInGroup, db
 from forms.editForm import EditTeacherForm
 
 teachers_blueprint = Blueprint("teachers_blueprint", __name__)
@@ -23,7 +24,6 @@ add_teachers_blueprint = Blueprint("add_teachers_blueprint", __name__)
 def add_teachers():
     addTeacherFormData = AddTeacherForm()
     # teachers = db.session.query(Teacher).all()
-    group_of_teachers = db.session.query(GroupOfTeacher).all()
     if True:  # request.method == "POST":
         if addTeacherFormData.validate_on_submit():
             teacherData = Teacher()
@@ -39,8 +39,7 @@ def add_teachers():
 
         else:
             return render_template("teachers/addTeacherForm.html",
-                                   form=addTeacherFormData,
-                                   group_of_teachers=group_of_teachers)
+                                   form=addTeacherFormData)
 
 
 show_edit_teachers_blueprint = Blueprint(
@@ -50,6 +49,8 @@ show_edit_teachers_blueprint = Blueprint(
 @show_edit_teachers_blueprint.route("/teachers/edit")
 def show_edit_teachers():
     editTeacherFormData = EditTeacherForm()
+    global current_data
+    current_data = editTeacherFormData
     # itemId auslesen
     teacher_Id = request.args["teacher_Id"]
     # Item laden
@@ -62,7 +63,12 @@ def show_edit_teachers():
     editTeacherFormData.birthdate.data = teacher_to_edit.birthdate
     # editTeacherFormData.teacher_in_group_Id.data = teacher_to_edit.teacher_in_group_Id
 
-    return render_template("/teachers/editTeacherForm.html", form=editTeacherFormData)
+    group_of_teachers = db.session.query(GroupOfTeacher).filter(
+        sqlalchemy.and_(TeacherInGroup.group_of_teachers_Id == GroupOfTeacher.group_of_teachers_Id,
+                    TeacherInGroup.teacher_Id == teacher_Id))
+
+    return render_template("/teachers/editTeacherForm.html", form=editTeacherFormData,
+                           group_of_teachers=group_of_teachers)
 
 
 submit_edit_teachers_blueprint = Blueprint(
@@ -72,7 +78,7 @@ submit_edit_teachers_blueprint = Blueprint(
 @submit_edit_teachers_blueprint.route("/teachers/edit", methods=["post"])
 def submit_edit_teachers():
     editTeacherFormData = EditTeacherForm()
-
+    create_report_file(editTeacherFormData)
     if editTeacherFormData.validate_on_submit():
         # daten aus Form auslesen
         teacher_Id = editTeacherFormData.teacher_Id.data
@@ -90,3 +96,50 @@ def submit_edit_teachers():
 
     else:
         raise("Fatal Error")
+
+import pdfkit
+
+
+def create_report_file(teacher_data):
+    table_html = """<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table, th, td {
+    border: 1px solid black;
+    }
+    
+    table {
+    width: 100%;
+    }
+    </style>
+    </head>
+    <body>
+    
+    <h2>Sample Table</h2>
+    
+    <table>
+    <tr>
+        <th>Field 1</th>
+        <th>Field 2</th>
+    </tr>
+    <tr>
+        <td>x1</td>
+        <td>x2</td>
+    </tr>
+    <tr>
+        <td>x3</td>
+        <td>x4</td>
+    </tr>
+    </table>
+    
+    </body>
+    </html>
+    """
+    
+    pdfkit.from_string(table_html, output_path="sample_table.pdf")
+    """f = open("test.txt", 'w')
+    f.write("old " + current_data.first_name.data + "\n")
+    f.write("\n")
+    f.write("new " + teacher_data.first_name.data)
+    f.close()"""
